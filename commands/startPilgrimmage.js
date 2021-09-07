3f7277ea44afb791e777c2871246a8c8d35fc0aa
Wei Sheng Aw <weisheng_aw@hotmail.com>
2021-09-06 23:28:08 -0700

    lowercase support

--- commands/startPilgrimmage.js
+++ commands/startPilgrimmage.js
@@ -1,10 +1,11 @@
 const Increment = require('../mongoFunctions/Increment.js');
 const { pilgrimRoleId, weisCornerId } = require('../config.json');
+const events = require('events');
 
 module.exports = {
     name: 'start',
     description: 'Start the pilgrimmage!',
-    aliases: [],
+    aliases: ['st'],
     args: 0,
     usage: "<number><unit (s,m,h)>",
     cooldown: 5,
@@ -13,13 +14,14 @@ module.exports = {
         if (!message.member.roles.cache.some(role => role.name === 'pilgrim master' || role.name === 'mod')) return;
 
         var timer = 0;
+        var suffix = args[0].toLowerCase()
         if (args.length === 1 && (/^\d+\D$/i).test(args[0])) {
-            if (args[0].endsWith('s')) {
-                timer = parseInt(args[0].substring(0, args[0].length - 1));
-            } else if (args[0].endsWith('m')) {
-                timer = parseInt(args[0].substring(0, args[0].length - 1)) * 60;
-            } else if (args[0].endsWith('h')) {
-                timer = parseInt(args[0].substring(0, args[0].length - 1)) * 3600;
+            if (suffix.endsWith('s')) {
+                timer = parseInt(args[0].substring(0, args[0].length - 1)) * 1000;
+            } else if (suffix.endsWith('m')) {
+                timer = parseInt(args[0].substring(0, args[0].length - 1)) * 60000;
+            } else if (suffix.endsWith('h')) {
+                timer = parseInt(args[0].substring(0, args[0].length - 1)) * 3600000;
             } else {
                 message.channel.send('invalid time suffix, use s, m, or h');
                 return;
@@ -28,24 +30,24 @@ module.exports = {
             message.channel.send('invalid usage, use a number followed by s, m, or h');
             return;
         }
-        message.channel.send('<@&' + pilgrimRoleId + '>');
-        setTimeout(() => {
-            const channels = message.guild.channels.cache.filter(c => c.id === weisCornerId);
-            for (const [channelID, channel] of channels) {
-                for (const [memberID, member] of channel.members) {
-                    opts = {
-                        userId: memberID,
-                        username: member.user.tag,
-                        nickname: member.displayName
-                    }
-                    Increment.increment(opts, (res) => {
-                        if (res && res.error) {
-                            message.channel.send('Error, not recorded for ' + member.user.username);
-                        }
-                    })
-                }
+        message.channel.send('<@&' + pilgrimRoleId + '>' + ' approx ' + args[0]);
+
+        var startTime = new Date().getTime();
+        var fiveminflag = true;
+        var oneminflag = true;
+        var interval = setInterval(function(){
+            if(new Date().getTime() - startTime > timer) {
+                clearInterval(interval);
+                return;
             }
-            message.channel.send('Pilgrimmage recorded');
-        }, timer * 1000);
+            if(timer - (new Date().getTime() - startTime) < 300000 && timer > 300000 && fiveminflag) {
+                fiveminflag = false;
+                message.channel.send('approx 5m');
+            }
+            if(timer - (new Date().getTime() - startTime) < 60000 && timer > 60000 && oneminflag) {
+                oneminflag = false;
+                message.channel.send('approx 1m');
+            }
+        }, 3000)
     },
 }
